9e030dea23314da9e39e9864dcff7f0ec6d60c2f
calebphillips <caleb.phillips@gmail.com>
2013-09-12 20:28:09 -0500

    Clean up perm

--- _calebp+clojure/perm.clj
+++ _calebp+clojure/perm.clj
@@ -1,30 +1,25 @@
-(defn symbols [n]
-  (mapv #(symbol (str "x" %))
-        (range 1 (inc n))))
-
-(defn generate-for-bindings [n]
-  (let [symbols (symbols n)
-        numbers (range 1 (inc n))
-        pairs (for [i (range n)]
-                [(nth symbols i)
-                 (set (subvec symbols 0 i))])]
-    (mapcat (fn [[x priors]]
-              `[~x (remove ~priors ~(vec numbers))]) pairs)))
 
 ;; First thing that came to mind for generating combinations was a for
 ;; loop.  Then I had to figure out if I could dynamically generate a
 ;; for loop with n bindings.  So... a macro.  Probably not the best
-;; solution, but fun.  This macro has the
+;; solution, but fun.  This macro has a
 ;; problem when it is passed a symbol instead of a literal number, but
-;; I have to get to get back to work
+;; I have to get back to work
 (defmacro perms [n]
-  `(for ~(vec (generate-for-bindings n))
-     ~(vec (symbols n))))
+  (let [numbers (vec (range 1 (inc n)))
+        symbols (mapv #(symbol (str "x" %))
+                      numbers)
+        pairs (for [i (range n)]
+                [(nth symbols i)
+                 (set (subvec symbols 0 i))])
+        binding-forms (mapcat (fn [[x priors]]
+                                `[~x (remove ~priors ~numbers)])
+                              pairs)]
+    `(for ~(vec binding-forms)
+       ~(vec symbols))))
 
 (comment
-  (let [ps (perms 3)]
+  (let [ps (perms 4)]
     (println (count ps))
     (doseq [p ps]
       (println (clojure.string/join " " p)))))
-
-
