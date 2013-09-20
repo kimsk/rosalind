1250b04fca98520c95d26eb4c535b1ffa568e254
Mark Wutka <woolman>
2013-09-20 18:56:52 -0500

    Added solution for SORT

---
+++ mark_wutka+haskell/SORT/make_table.hs
@@ -0,0 +1,41 @@
+import System.IO
+import Data.List
+import qualified Data.Map as Map
+import Debug.Trace
+
+-- Constructs a lookup table with a permutation and the number of
+-- reversals needed to get from 0123456789 to that permutation.
+-- We start with just ("0123456789",0) in the table, then try every
+-- reversal on it, and add the resulting permutations with a reversal
+-- count of 1. Then we take all the permutations with a reversal count of 1,
+-- apply all the reversals on them to generate the ones with a count of 2.
+-- We continue this until we look for reversals with a particular count
+-- and there aren't any.
+
+doReversal s start length =
+    (take start s) ++ (reverse (take length (drop start s))) ++ (drop (start+length) s)
+
+computeReversalsTable reversalTable depth =
+    trace ("At depth "++(show depth)++" want to try "++(show $ length reversalsToTry)++" reversals")
+    (if length reversalsToTry > 0 then
+        computeReversalsTable (foldl' tryReversals reversalTable reversalsToTry) (depth+1)
+    else
+        reversalTable)
+    where
+        reversalsToTry = filter (compareReversalDepth depth) $ Map.toList reversalTable
+        compareReversalDepth depth (k,v) = depth == (fst v)
+        tryReversals reversalTable (reversal, (revDepth,revList)) =
+            foldl' tryNewReversal reversalTable [(doReversal reversal s l,s+1,l+s,revList) | l <- [2..10], s <- [0..(10-l)]]
+        tryNewReversal reversalTable (attemptedReversal,start,length,revList) =
+            if not $ Map.member attemptedReversal reversalTable then
+                Map.insert attemptedReversal (depth+1,[start,length]:revList) reversalTable
+            else
+                reversalTable
+
+makeTableLines reversalTable =
+    map makeTableLine $ Map.toList reversalTable
+    where
+        makeTableLine (reversal, (depth,revList)) = reversal ++ " " ++ (show (reverse revList))
+
+main = do
+    writeFile "reversal_table.txt" (unlines $ makeTableLines (computeReversalsTable (Map.fromList [ ("0123456789", (0,[])) ]) 0))
---
+++ mark_wutka+haskell/SORT/sort.hs
@@ -0,0 +1,43 @@
+import System.IO
+import System.Environment
+import Text.Printf
+import Data.List
+import Data.List.Split
+import qualified Data.Map as Map
+import Debug.Trace
+
+-- This solution uses a pre-computed reversal table that contains all
+-- permutations of 01234567890 and the reversals it takes to get from
+-- 0123456789 to that permutation.
+--
+-- To find the reversals from any arbitrary list of 10 elements to
+-- another list of the same elements, we just have to map those elements
+-- into 0123456789. Specifically, we create a map from the target
+-- elements into 0123456789 using the "from" list, and then apply the
+-- map to the elements of the "to" list. This returns the permutation
+-- of 0123456789 that we need to look for in the table.
+
+makeReversalTable tableLines =
+    Map.fromList $ map (makeTableEntries . words) tableLines
+    where
+        makeTableEntries :: [String] -> (String,[[Int]])
+        makeTableEntries [perm,reversals] = (perm,read reversals)
+
+findReversal :: Map.Map String [[Int]] -> [String] -> [[Int]])
+findReversal reversalTable [fromStr, toStr] =
+    (Map.findWithDefault [[-1,-1]] computeTarget reversalTable)
+    where
+        from = words fromStr
+        to = words toStr
+        computeTarget = map getTargetChar to
+        getTargetChar ch = Map.findWithDefault '?' ch targetMapping
+        targetMapping = Map.fromList (zip from "0123456789")
+
+main = do
+  argv <- getArgs
+  reversalTableContents <- readFile "reversal_table.txt"
+  let reversalTable = makeReversalTable $ lines reversalTableContents
+  fileLines <- readFile (head argv)
+  let reversals = findReversal reversalTable $ take 2 (lines fileLines)
+  putStrLn . show $ length reversals
+  putStr . unlines $ map (\r -> unwords $ map show r) reversals
